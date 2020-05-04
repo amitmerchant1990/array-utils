78890a91c68972703753e80381c6450d47148730
amitmerchant1990 <bullredeyes@gmail.com>
2020-05-04 17:57:14 +0530

    add docblocks.

--- src/ArrayUtils.php
+++ src/ArrayUtils.php
@@ -6,11 +6,22 @@ class ArrayUtils
 {
     public $collection;
 
+    /**
+     * Returns the class instance
+     *
+     * @return ArrayUtils
+     */
     public static function getInstance()
     {
         return new ArrayUtils();
     }
 
+    /**
+     * Collects the input array
+     *
+     * @param array $collection
+     * @return $this
+     */
     public function collect(array $collection)
     {
         $this->collection = $collection;
@@ -18,31 +29,65 @@ class ArrayUtils
         return $this;
     }
 
+    /**
+     * Wrapper method for array_map
+     *
+     * @param \Closure $closure
+     * @return mixed
+     */
     public function map(\Closure $closure)
     {
         return array_map($closure, $this->collection);
     }
 
+    /**
+     * Wrapper method for array_filter
+     *
+     * @param \Closure $closure
+     * @return array
+     */
     public function filter(\Closure $closure)
     {
         return array_filter($this->collection, $closure);
     }
 
+    /**
+     * Wrapper method for in_array
+     *
+     * @param $item
+     * @return bool
+     */
     public function contains($item): bool
     {
         return in_array($item, $this->collection);
     }
 
+    /**
+     * Wrapper method for array_values
+     *
+     * @return array
+     */
     public function getValues()
     {
         return array_values($this->collection);
     }
 
+    /**
+     * Wrapper method for array_keys
+     *
+     * @return array
+     */
     public function getKeys()
     {
         return array_keys($this->collection);
     }
 
+    /**
+     * Wrapper method for array_search
+     *
+     * @param string $searchParam
+     * @return false|int|string
+     */
     public function search(string $searchParam)
     {
         return array_search($searchParam, $this->collection);
